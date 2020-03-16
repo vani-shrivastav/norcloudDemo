8021b7161d95799fbff5132f264ff318b2c1c711
vani-shrivastav <shriv40@gmail.com>
2020-03-16 17:24:00 +0000

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -6,6 +6,7 @@ resource "azurerm_virtual_network" "demoVnet" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+    
 }
 
 resource "azurerm_subnet" "demoSubnet" {
